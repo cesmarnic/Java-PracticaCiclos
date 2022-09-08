49e2da97ccb68025f1c957c81c66103cc23c719c
Octavio Enrique Morales García <89498121+cesmarnic@users.noreply.github.com>
2022-09-08 13:13:53 -0600

    ..

---
+++ Alumno.java
@@ -0,0 +1,10 @@
+public class Alumno extends Persona{
+
+    public String Grado;
+    public String Seccion;
+    public ArrayList<String> Asignaturas;
+    
+    public void Estudiar(){
+
+    }
+}
---
+++ Persona.java
@@ -0,0 +1,24 @@
+public class Persona{
+
+    public String nombre;
+    public int edad;
+    public String genero;
+    public double altura;
+    public double peso;
+
+    public void Respirar(){
+
+    }
+
+    public void Caminar(){
+
+    }
+
+    public void Comer(){
+
+    }
+
+    public void Hablar(){
+        
+    }
+}
